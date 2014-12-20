b4e6f05569aa0cac2a4ba9afab9d8ed4bba466c0
Александр Коваленко <merlinwt@gmail.com>
2014-12-20 16:06:05 +0300

    hotel

---
+++ js/datetimep.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Администратор on 20.12.2014.
+ */
+$(function(){
+    $('.dtp').datepicker({
+        language: "ru",
+        autoclose: true,
+        todayHighlight: true
+    });
+});
