34a1df5b10fb0c896b8afc7680e7c78449d99e78
Александр Коваленко <merlinwt@gmail.com>
2014-12-21 23:39:33 +0300

    php + mySql class + animal_list

---
+++ php/animals_in_valler.php
@@ -0,0 +1,11 @@
+<?
+error_reporting(E_ALL);
+require "./classes/mysql.php";
+define("sql_s_animal_in_vallere","select * from v_curent_animals");
+
+$db = new SafeMySQL();
+$data = $db->getAll(sql_s_animal_in_vallere);
+$data = json_encode($data);
+
+var_dump($data);
+?>
